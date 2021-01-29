eb349b19d2dfab0c404d92833210524ea6f8e7b4
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2021-01-28 23:32:13 -0300

    archivos

---
+++ app/models/pet.rb
@@ -0,0 +1,25 @@
+class Pet < ApplicationRecord
+
+  has_many :pet_histories
+
+  def history_count
+    self.pet_histories.count
+  end
+
+  def avg_weight
+    #TODO-implement
+  end
+
+  def avg_height
+    #TODO-implement
+  end
+
+  def max_weight
+    #TODO-implement
+  end
+
+  def max_height
+    #TODO-implement
+  end
+
+end
